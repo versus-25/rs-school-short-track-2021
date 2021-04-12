987b280afc832ab9b50ecd1b5ecc1fe0ab6bd051
versus-25 <ivan_schkola@mail.ru>
2021-04-12 21:03:11 +0300

    feat: implement encode line function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -9,10 +9,26 @@
  *
  */
 function encodeLine(str) {
-  // const listStr = str.split('');
-  // listStr.forEach(x);
-  // return listStr.for;
-  return str;
+  if (str === '') return '';
+  let res = '';
+  const listStr = str.split('');
+  let prevChar = '';
+  let cnt = 0;
+  listStr.forEach((char, index) => {
+    if (char === prevChar) {
+      cnt++;
+    } else if (prevChar === '') {
+      cnt = 1;
+    } else {
+      res += (cnt > 1 ? String(cnt) : '') + prevChar;
+      cnt = 1;
+    }
+    if (index === listStr.length - 1) {
+      res += (cnt > 1 ? String(cnt) : '') + char;
+    }
+    prevChar = char;
+  });
+  return res;
 }
 
 module.exports = encodeLine;
