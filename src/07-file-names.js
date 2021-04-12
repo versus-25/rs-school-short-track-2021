e0715ba233b5908ff48bf80cd97220532e4c2088
versus-25 <ivan_schkola@mail.ru>
2021-04-12 10:43:16 +0300

    feat: fix filename rename and add email domain function

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,35 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const res = [];
+  const reg = /\([\d]{1,}\)$/;
+  names.forEach((item) => {
+    let nameFile = '';
+    if (res.includes(item)) {
+      if (reg.test(item)) {
+        const suffix = item.match(reg)[0];
+        nameFile = item + suffix;
+      } else {
+        const prevNames = res.filter((x) => item === x.slice(0, x.length - 3));
+        if (prevNames.length !== 0) {
+          const prevMaxname = [...prevNames].sort((a, b) => {
+            const getIndex = (x) => x.match(reg)[0].match(/[\d]{1,}/)[0];
+            return getIndex(b) - getIndex(a);
+          })[0];
+          nameFile = `${item}(${String(
+            +prevMaxname.match(/[\d]{1,}/)[0] + 1,
+          )})`;
+        } else {
+          nameFile = `${item}(1)`;
+        }
+      }
+    } else {
+      nameFile = item;
+    }
+    res.push(nameFile);
+  });
+  return res;
 }
 
 module.exports = renameFiles;
--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const strlist = email.split('@');
+  return strlist[strlist.length - 1];
 }
 
 module.exports = getEmailDomain;
